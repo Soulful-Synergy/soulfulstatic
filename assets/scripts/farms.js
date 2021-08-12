4e55c792de567c8d9f812bda9d233540f2925b8b
jahedev <jahedev@gmail.com>
2021-08-12 11:59:19 -0400

    carousel almost finished

---
+++ assets/scripts/farms.js
@@ -0,0 +1,46 @@
+const caro_1 = '#site-carousel-1';
+const caro_2 = '#site-carousel-2';
+const caro_3 = '#site-carousel-3';
+const caro_4 = '#training-carousel';
+
+const siteCarousel_1 = new Glide(caro_1, {
+    type: 'carousel',
+    perView: 1,
+    autoplay: 6000,
+}).mount();
+
+const siteCarousel_2 = new Glide(caro_2, {
+    type: 'carousel',
+    perView: 1,
+    autoplay: 7000,
+}).mount();
+
+const siteCarousel_3 = new Glide(caro_3, {
+    type: 'carousel',
+    perView: 1,
+    autoplay: 8000,
+}).mount();
+
+const trainingCarousel = new Glide(caro_4, {
+    type: 'carousel',
+    perView: 4,
+    breakpoints: {
+        1600: { perView: 3 },
+        1215: { perView: 2 },
+        900: { perView: 1 },
+    },
+    autoplay: 6000,
+}).mount();
+
+function addBullets(glider) {
+    console.log(glider.children);
+}
+
+window.onload = (e) => {
+    const glider1 = document.querySelector(caro_1);
+    const glider2 = document.querySelector(caro_2);
+    const glider3 = document.querySelector(caro_3);
+    [glider1, glider2, glider3].forEach((glider) => {
+        addBullets(glider);
+    });
+};
