37995b7bf544d0e35950f1db41fd7787c55aa27d
jahedev <jahedev@gmail.com>
2021-08-13 17:23:00 -0400

    only contact part left

--- assets/scripts/farms.js
+++ assets/scripts/farms.js
@@ -34,43 +34,109 @@ const trainingCarousel = new Glide(caro_4, {
     autoplay: 6000,
 }).mount();
 
-function addBullets(glider) {
-    // console.log(glider.children);
-}
-
-window.onload = (e) => {
-    const glider1 = document.querySelector(caro_1);
-    const glider2 = document.querySelector(caro_2);
-    const glider3 = document.querySelector(caro_3);
-    [glider1, glider2, glider3].forEach((glider) => {
-        addBullets(glider);
-    });
-};
-
 /* caleandar.js */
 const eventsList = document.querySelector('#events-list');
-for (const child of eventsList.children) {
-    if (child.className !== 'list-event') {
+
+let cleandarId = document.getElementById('caleandar');
+let events = [];
+let settings = {};
+
+for (const listEvent of eventsList.children) {
+    const date = listEvent.dataset.date;
+    const title = listEvent.dataset.title;
+    const eventURL = listEvent.dataset.url;
+
+    if (listEvent.className !== 'list-event' || !date) {
+        console.error('Error: invalid list event or undefined date.');
         continue;
+    }
+    console.log(date);
+
+    // Date must me in the format of YYYY-MM-DD
+    let dateReg = /[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}/;
+    if (!dateReg.test(date)) {
+        console.error(
+            'Error: invalid date for list event. It should be (YYYY-MM-DD).'
+        );
+    }
+
+    const splitDate = date.split('-');
+    const year = parseInt(splitDate[0]);
+    const month = parseInt(splitDate[1]) - 1; // -1 because month starts with index 0
+    const day = parseInt(splitDate[2]);
+
+    console.log(new Date(year, month, day));
+
+    events.push({
+        Date: new Date(year, month, day),
+        Title: title || 'Untitled Event',
+        Link: eventURL || 'javascript:void(0)',
+    });
+}
+
+caleandar(cleandarId, events, settings);
+
+/* Modal */
+
+const modals_container = document.querySelector('.modals-container');
+const modals = document.querySelectorAll('.modal');
+const modalOpeners = document.querySelectorAll('[data-modalid]');
+
+let modalOpen = false;
+
+document.querySelectorAll('.modal-button-close button').forEach((exitBtn) => {
+    exitBtn.addEventListener('click', (e) => {
+        modals.forEach((modal) => {
+            showElement(modal, false);
+        });
+        showElement(modals_container, false);
+        applyBackgroundEffects(false);
+        modalOpen = false;
+    });
+});
+
+modalOpeners.forEach((modalOpener) => {
+    const modalId = modalOpener.dataset.modalid;
+
+    modalOpener.addEventListener('click', (e) => {
+        if (modalOpen) return;
+
+        const modal = document.querySelector(`#modal-${modalId}`);
+
+        // if modal does not exist, then return
+        if (!modal) return;
+
+        applyBackgroundEffects(true);
+        showElement(modals_container, true);
+        showElement(modal, true);
+        modalOpen = true;
+    });
+});
+
+function showElement(el, shouldShow = true) {
+    if (shouldShow) {
+        el.classList.remove('hidden');
+    } else {
+        el.classList.add('hidden');
     }
 }
 
-/* Instagram Feed */
+function applyBackgroundEffects(shouldShow) {
+    const header = document.querySelector('header');
+    const farms_page = document.querySelector('.farms-page');
+    const body = document.querySelector('body');
 
-const mediaDiv = document.querySelector('.farms-ig-media-wrapper');
+    if (shouldShow) {
+        header.classList.add('dim-blur');
+        farms_page.classList.add('dim-blur');
+        body.classList.add('disable-overflow-y');
+    } else {
+        header.classList.remove('dim-blur');
+        farms_page.classList.remove('dim-blur');
+        body.classList.remove('disable-overflow-y');
+    }
+}
 
-let igToken =
-    'IGQVJXWDhBVklhclNTYlNOMXhscXV0dFJCMG50X3lHZAzhvMlhKYmdzZAlNZAYmNVTHBmZAHR0bFRra3d4X1IzMEktRG5xRnpQVTFFWjYycjBCSWxuWmZAwOXNSRkIwc1dTaTNOckQxSTBTNlBFVzF1VXNUZAAZDZD';
-
-let ig;
-
-// fetch('https://www.instagram.com/_soulfulsynergy_/?__a=1')
-//     .then((response) => response.json())
-//     .then((data) => (ig = data));
-
-// console.log(ig);
-
-var feed = new Instafeed({
-    accessToken: igToken,
-});
-feed.run();
+function openEventModal() {
+    console.log('modal opened');
+}
